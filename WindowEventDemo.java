f728edadedb801f5a9cf19961894c57634f6367c
Gu Lab <UWGULABSERVER@gmail.com>
2018-05-07 17:12:31 -0700

    swtich off valve at end of sequencing

--- WindowEventDemo.java
+++ WindowEventDemo.java
@@ -254,7 +254,7 @@ public class WindowEventDemo extends javax.swing.JFrame {
             } else {
                 throw new IllegalArgumentException("cannot run negative number of incorporation cycles.");
             }
-            experiment.getSelector().switchValve(01);
+            experiment.getSelector().switchValve(20);
             gui_.closeSequence(true);
             gui_.closeAllAcquisitions();
 
